3d3be70ae2edcfa188986766d5a65ba60bccbbaa
Crisna Bezerra <102383703+crisnabto@users.noreply.github.com>
2022-11-13 21:36:32 -0300

    Update index.js

--- crisna-bezerra-portfolio/src/index.js
+++ crisna-bezerra-portfolio/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+// import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from "react-router-dom";
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
@@ -8,9 +9,9 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <HashRouter base="/">
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
 
